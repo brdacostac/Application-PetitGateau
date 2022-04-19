203bb859fba581945b12dc2272d07af7bb1d3741
runtenick <runtenick2@gmail.com>
2022-04-19 20:01:49 +0200

    save

---
+++ InterfaceAppli1/UCrecette.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace InterfaceAppli1
+{
+    /// <summary>
+    /// Interação lógica para UCrecette.xam
+    /// </summary>
+    public partial class UCrecette : UserControl
+    {
+        public UCrecette()
+        {
+            InitializeComponent();
+        }
+
+        public string ImageName
+        {
+            set
+            {
+                ImageR.Source = new BitmapImage(new Uri(value,UriKind.Relative));
+            }
+        }
+
+        public string NomRecette
+        {
+            set 
+            {
+                NomR.Text = value;
+            }
+        }
+
+        public string LikeLiked
+        {
+            set
+            {
+                LikeButton.Source = new BitmapImage(new Uri(value, UriKind.Relative));
+            }
+        }
+    }
+}
