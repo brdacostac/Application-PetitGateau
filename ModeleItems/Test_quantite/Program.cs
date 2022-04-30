12b8004471ca8419747a143b6660f25835fd090a
runtenick <runtenick2@gmail.com>
2022-04-30 19:41:42 +0200

    c# start

---
+++ ModeleItems/Test_quantite/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ModeleItems;
+
+namespace Test_quantite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test de la classe Quantite");
+
+            Quantité qte = new Quantité(20,Unité.KG);
+            Console.WriteLine(qte);
+        }
+    }
+}
